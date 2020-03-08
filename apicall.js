cd3f426ccac93ef9849b92e1ab4fbd48da14f9d0
A Gadon <apgadon@gmail.com>
2020-03-08 10:13:55 -0700

    Added sentiment connection to apicall.js

--- apicall.js
+++ apicall.js
@@ -1,8 +1,65 @@
-async function start(){
-	const result = await myfunction();
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+//start server
+var server = http.createServer(function(request, response)
+{
+	//get url for server
+	var path = url.parse(request.url).pathname;
+	switch (path)
+	{
+		//if apicall detected, get it ready for a post request from officehours.html
+		case '/apicall':
+			let body = "";
+			request.on('data', chunk => {
+				body += chunk.toString();
+			});
+			request.on('end', () =>{
+				analyzeSentiment(body);
+				console.log(body);
+				response.end('ok');
+			});
+			break;
+		//if html page detected, return page
+		case '/officehours.html':
+			fs.readFile(__dirname + path, function(error, data)
+			{
+				if (error)
+				{
+					response.writeHead(404);
+					response.writeError(error);
+					response.end();
+				}
+				else
+				{					
+					response.writeHead(200, {'Content-Type': 'text/html'});
+					response.write(data);
+					response.end();
+				}
+			})
+			break;
+		default:
+			response.writeHead(404);
+			response.write("Oops, this doesn't exist - 404");
+			response.end();
+			break;
+	}
+});
+server.listen(8082);
+
+function analyzeSentiment(message)
+{
+	//extract "string=" string from message
+	message.substring(7);
+	start()
 }
 
-const myfunction = async function()
+async function start(){
+	const result = await sentimentFunction();
+}
+
+const sentimentFunction = async function()
 {
 	// Imports the Google Cloud client library
 	const language = require('@google-cloud/language');
@@ -10,10 +67,7 @@ const myfunction = async function()
 	// Creates a client
 	const client = new language.LanguageServiceClient();
 
-	/**
-	 * TODO(developer): Uncomment the following line to run this code.
-	 */
-	const text = 'Your text to analyze, e.g. Hello, world!';
+	const text = 'and i oop';
 
 	// Prepares a document, representing the provided text
 	const document = {
@@ -37,4 +91,3 @@ const myfunction = async function()
 	});
 }
 
-start();
